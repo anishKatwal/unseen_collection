6e5951381e292d7b1a3f8c1ff9335b0285ee4afd
AnishCollege <np05cp4s230011@iic.edu.np>
2025-04-30 09:28:13 +0545

    Final commit

--- app/src/api/booking.js
+++
@@ -1,8 +0,0 @@
-// src/api/bookings.js
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://localhost:5000/api/table-orders", // update if needed
-});
-
-export const createBooking = (data) => API.post("/", data);
