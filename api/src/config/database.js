f3f342c006bd338f2ba5cba71b54d52338354058
anishKatwal <anishkatwal29@gmail.com>
2025-09-02 23:43:25 -0700

    First commit to this repository

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || "");
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("MONGO_URI from env:", process.env.MONGO_URI); // 👈 Add this
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   }
 };
